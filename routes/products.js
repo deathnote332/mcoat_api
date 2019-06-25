59528f3ce268e6652065c88ad64c92c3f93380bf
Paul <jpinhog@indracompany.com>
2019-06-25 11:09:15 +0800

    Ready for heroku

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const Users = require('../model/user');
+const {Users} = require('../model/user');
 
-router.get('/',async (req,res)=>{
+router.get('/',auth,async (req,res)=>{
     products = await Products.findAll({attributes: ['id', 'brand','description']})
     res.json(products);
 })
