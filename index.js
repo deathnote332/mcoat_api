d94ae5b6a61d5ae26da9b4bc53c9131c095281b6
Paul <jpinhog@indracompany.com>
2019-06-25 13:46:31 +0800

    added body-parser

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
+
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const express = require('express');
 const app = express();
 app.use(cookieParser())
 app.use(cors())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 require('dotenv').config()
 require('./startup/logger')();
