ed449be44522cbbcd1e29be645e669369af663d7
Paul <jpinhog@indracompany.com>
2019-06-25 13:28:50 +0800

    remove operatorsAliases

--- startup/connection.js
+++ startup/connection.js
@@ -4,7 +4,6 @@ const winston = require('winston');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    operatorsAliases: false,
 });
 
 sequelize.authenticate()
