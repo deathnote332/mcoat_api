a6478d201b9c93441b7534e4d56ba26366d91d5b
Paul <jpinhog@indracompany.com>
2019-06-25 13:39:33 +0800

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const app = express();
 app.use(cookieParser())
+app.use(cors())
 
 require('dotenv').config()
 require('./startup/logger')();
--- startup/connection.js
+++ startup/connection.js
@@ -3,7 +3,7 @@ const winston = require('winston');
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    dialect: 'mysql'
 });
 
 sequelize.authenticate()
