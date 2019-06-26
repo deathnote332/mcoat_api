1b4bf03426b62f96167e6c9e2164a5316ed5dd41
Paul <jpinhog@indracompany.com>
2019-06-26 17:48:08 +0800

    update

--- routes/auth.js
+++ routes/auth.js
@@ -8,18 +8,18 @@ const {User,generateToken} = require('../model/user');
 
 router.post('/', async  (req,res)=>{
     const { error } = validate(req.body);
-    if(error) return res.status(400).json({
+    if(error) return res.status(422).json({
         isSuccessful: false,
         message: error.details[0].message
     });
 
     let user = await User.scope('is_deleted').findOne({where:{email: req.body.email}});
-    if(_.isEmpty(user)) return res.status(400).json({isSuccessful: false, message: "Invalid email or password"});
+    if(_.isEmpty(user)) return res.status(422).json({isSuccessful: false, message: "Invalid email or password"});
 
     let password = user.password;
 
     const isValidPass = await bcrypt.compare(req.body.password,password);
-    if(!isValidPass) return res.status(400).json({isSuccessful: false, message: "Invalid email or password"});
+    if(!isValidPass) return res.status(422).json({isSuccessful: false, message: "Invalid email or password"});
   
     user = _.pick(user,['first_name','last_name','email','status','user_type','warehouse','active']);
     res.cookie('token',generateToken(user), {
