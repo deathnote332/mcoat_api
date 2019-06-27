c86de963e012b98a221ce87c906a0786b8753520
Paul <jpinhog@indracompany.com>
2019-06-27 11:26:36 +0800

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req,res,next){
+    if (!req.headers.authorization) return res.status(401).send(`Access denied. No Token provided`);
     const token = req.headers.authorization.split(" ")[1]
-    
-    if(!token) return res.status(401).send(`Access denied. No Token provided ${token}`);
+    if(!token) return res.status(401).send(`Access denied. No Token provided`);
     console.log(process.env.SECRET_TOKEN)
     try{
         const decoded = jwt.verify(token,process.env.SECRET_TOKEN);
