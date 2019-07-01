349aedbb6165ce1a013abd3d9dfe366a0e45f9a2
Paul <jpinhog@indracompany.com>
2019-07-01 15:33:23 +0800

    git push

--- routes/products.js
+++ routes/products.js
@@ -4,9 +4,33 @@ const auth = require('../middleware/auth');
 const { Products } = require('../model/product');
 
 router.get('/',auth ,async (req,res)=>{
-    products = await Products.findAll({attributes: ['id', 'brand','description']});
+    products = await Products.findAll();
     res.json(products);
 })
 
+router.post('/', auth, async(req,res) => {
+    console.log(req);
+    product = await Products.findOne({
+        where: {id: req.body.id}
+    })
+    res.json(product);
+});
+
+router.get('/brands/:page',auth ,async (req,res)=>{
+    const limit = 10;
+    let offset = 0;
+    const page = req.params.page;
+    const dataCount = Products.findAndCountAll({group: 'brand'});
+    const pages = Math.ceil(dataCount / limit);
+    offset = limit * (page - 1);
+
+    brands = await Products.findAll({
+        group: 'brand', 
+        attributes: ['brand'],
+        limit,
+        offset
+    });
+    res.json(brands);
+})
 
 module.exports = router;
