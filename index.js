1a255099cc339cecdd8010c98a1eae040fdb5592
Paul <jpinhog@indracompany.com>
2019-07-17 17:47:27 +0800

    update

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
-app.use(cookieParser())
-app.use(cors())
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+// app.use(cookieParser())
+// app.use(cors())
 
-require('dotenv').config()
-require('./startup/logger')();
-require('./startup/route')(app);
+// require('dotenv').config()
+// require('./startup/logger')();
+// require('./startup/route')(app);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=> console.log(`Port : ${port}`));
+
+http.listen(port, function(){
+  console.log('listening on *:3000');
+});
+
+// app.listen(port,()=> console.log(`Port : ${port}`));
 
 
 
